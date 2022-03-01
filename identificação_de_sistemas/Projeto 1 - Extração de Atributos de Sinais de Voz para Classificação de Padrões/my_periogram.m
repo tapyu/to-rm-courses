37a6a696cfbab1d450dea05391ffae9012c97421
tapyu <rubem.engenharia@gmail.com>
2022-02-28 23:38:07 -0300

    fix: periogram loop

--- identificação_de_sistemas/Projeto 1 - Extração de Atributos de Sinais de Voz para Classificação de Padrões/my_periogram.m
+++ identificação_de_sistemas/Projeto 1 - Extração de Atributos de Sinais de Voz para Classificação de Padrões/my_periogram.m
@@ -1,16 +1,17 @@
-function [p, all_f] = my_periogram(x,Fs)
+function [p, all_f] = my_periogram(g,Fs)
 % This function immitates the built-in function
 % periogram()
 
 
-N = numel(x);
-delta_t = 1/Fs;
+N = numel(g);
 % Nyquist theorem
 W = Fs/2;
 all_f = 0:Fs/N:W;
 
-% two-sided periogram -> see doc periodogram
-p =  delta_t/N * abs(fft(x)).^2;
+% DFT
+G = fft(g);
+% two-sided periogram -> see doc guilherme slides
+p =  abs(G).^2 / (N*Fs);
 % transforming into one-sided
 p = p(1:N/2+1);
 p(2:end-1) = 2*p(2:end-1);
