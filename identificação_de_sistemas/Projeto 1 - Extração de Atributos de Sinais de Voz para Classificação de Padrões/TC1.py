6fac4a9b6ee3955315bc5c704e67fc659f4c6a3c
tapyu <rubem.engenharia@gmail.com>
2022-03-04 22:14:57 -0300

    feat: add noise

--- identificação_de_sistemas/Projeto 1 - Extração de Atributos de Sinais de Voz para Classificação de Padrões/TC1.py
+++ identificação_de_sistemas/Projeto 1 - Extração de Atributos de Sinais de Voz para Classificação de Padrões/TC1.py
@@ -11,10 +11,15 @@ import numpy as np
 from scipy.io import wavfile
 import wave
 import pyaudio
+from matplotlib import pyplot as plt
+
 
 a_avancar, a_esquerda, a_direita, a_parar, a_recuar = (np.empty(10) for _ in range(5))
 
 p = pyaudio.PyAudio()
+
+plt.plot()
+
 
 for command in ('avancar', 'direita', 'esquerda', 'parar', 'recuar'):
     for n in range(1,11):
