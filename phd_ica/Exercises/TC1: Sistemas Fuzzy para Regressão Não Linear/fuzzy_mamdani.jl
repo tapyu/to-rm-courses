8a722dc010078cadcb5aa3526f3ad3f1eeabb054
tapyu <rubem.engenharia@gmail.com>
2022-07-15 09:45:38 -0300

    [ICA]: TC1 Mamdani Fuzzy

--- phd_ica/Exercises/TC1: Sistemas Fuzzy para Regressão Não Linear/fuzzy_mamdani.jl
+++ phd_ica/Exercises/TC1: Sistemas Fuzzy para Regressão Não Linear/fuzzy_mamdani.jl
@@ -5,16 +5,21 @@ include("inference_mamdani.jl")
 
 ## Input and outputs
 𝐱, 𝐲 = FileIO.load("TC1data.jld2", "x", "y") # 𝐱 -> Inputs; 𝐲 -> Outputs
-𝐲_range = range(minimum(𝐲), maximum(𝐲), length(𝐲)) # Universo de discurso (?) da variavel de saida
-
-all_μy_B = hcat(map(output_fuzzification, range(minimum(𝐲), maximum(𝐲), length(𝐲)), fill(3, length(𝐲)))...)' # input fuzzification (all domain, all fuzzy set)
 
 # Mamdani fuzzy infer system
 𝐲̂ = rand(length(𝐲))
-for I ∈ (2,3)
-    for (n, xₙ) ∈ enumerate(𝐱)
-        μx_A = input_fuzzification(xₙ, I) # input fuzzification
-        𝐲̂[n] = inference(μx_A, all_μy_B, I, 𝐲_range) # compute ŷₙ
+for (K, 𝐲_range_max) ∈ zip((2,3), (190,150))
+    𝐲_range = range(0, 𝐲_range_max, length(𝐲)) # Universo de discurso (?) da variavel de saida
+    all_μx_A = hcat(map(input_fuzzification, range(minimum(𝐱), maximum(𝐱), length(𝐱)), fill(K, length(𝐱)))...)' # input fuzzification (all domain, all fuzzy set)
+    for (k, μx_A_k) ∈ enumerate(eachcol(all_μx_A)) # for each set
+        all_μx_A[:,k]/=maximum(μx_A_k) # normalize it to unity
+    end
+
+    all_μy_B = hcat(map(output_fuzzification, 𝐲_range, fill(K, length(𝐲)))...)' # output fuzzification (all domain, all fuzzy set)
+    all_μy_B/=maximum(all_μy_B) # normalize it to unity
+
+    for (n, (μx_A, xₙ)) ∈ enumerate(zip(eachrow(all_μx_A), 𝐱)) # for each input sample (already fuzzified)
+        𝐲̂[n] = inference(μx_A, all_μy_B, K, 𝐲_range, xₙ) # compute ŷₙ
     end
 end
 
--- phd_ica/Exercises/TC1: Sistemas Fuzzy para Regressão Não Linear/inference_mamdani.jl
+++ phd_ica/Exercises/TC1: Sistemas Fuzzy para Regressão Não Linear/inference_mamdani.jl
@@ -1,4 +1,4 @@
-function inference(μx_A, all_μy_B, I, 𝐲_range)
+function inference(μx_A, all_μy_B, I, 𝐲_range, xₙ)
     if I == 2 # for 2 set
         # 1th rule
         # IF (xₙ IS HIGH) OR (xₙ IS LOW)
@@ -12,7 +12,7 @@ function inference(μx_A, all_μy_B, I, 𝐲_range)
         # 2th rule
         # IF NOT((xₙ IS HIGH) OR (xₙ IS LOW))
         # THEN yₙ IS HIGH
-        μx_Ā = 0.017 - μx_A # apply the 1th rule -> A = ¬(A₁ ∨ A₂)
+        μx_Ā = 1 - μx_A # apply the 2th rule -> A = ¬(A₁ ∨ A₂)
         all_μy_B₂ = all_μy_B[:,2] # the output fuzzy set HIGH
         μAtoB⁽²⁾ = map(min, fill(μx_Ā, length(all_μy_B₁)), all_μy_B₂) # Modus Ponens
 
@@ -27,22 +27,22 @@ function inference(μx_A, all_μy_B, I, 𝐲_range)
         # IF xₙ IS MEDIUM
         # THEN yₙ IS HIGH
         μx_A₂ = μx_A[2] # the input fuzzy set MEDIUM
-        all_μy_B₃ = all_μy_B[:,3] # the output fuzzy set HIGH
-        μAtoB⁽¹⁾ = map(min, fill(μx_A₂, length(all_μy_B₃)), all_μy_B₃) # Modus Ponens 
+        μy_B₃ = all_μy_B[:,3] # the output fuzzy set HIGH
+        μAtoB⁽¹⁾ = map(min, fill(μx_A₂, length(μy_B₃)), μy_B₃) # Modus Ponens 
         
         # 2th rule
         # IF xₙ IS LOW
         # THEN yₙ IS MEDIUM
         μx_A₁ = μx_A[1] # the input fuzzy set LOW
-        all_μy_B₂ = all_μy_B[:,2] # the output fuzzy set MEDIUM
-        μAtoB⁽²⁾ = map(min, fill(μx_A₁, length(all_μy_B₂)), all_μy_B₂) # Modus Ponens
+        μy_B₂ = all_μy_B[:,2] # the output fuzzy set MEDIUM
+        μAtoB⁽²⁾ = map(min, fill(μx_A₁, length(μy_B₂)), μy_B₂) # Modus Ponens
 
         # 3th rule
         # IF xₙ IS HIGH
         # THEN yₙ IS LOW
         μx_A₃ = μx_A[3] # the input fuzzy set LOW
-        all_μy_B₁ = all_μy_B[:,1] # the output fuzzy set MEDIUM
-        μAtoB⁽³⁾ = map(min, fill(μx_A₃, length(all_μy_B₁)), all_μy_B₁) # Modus Ponens
+        μy_B₁ = all_μy_B[:,1] # the output fuzzy set MEDIUM
+        μAtoB⁽³⁾ = map(min, fill(μx_A₃, length(μy_B₁)), μy_B₁) # Modus Ponens
 
         # resulting output fuzzy set (aggregation)
         μAtoB = map(max, μAtoB⁽¹⁾, μAtoB⁽²⁾, μAtoB⁽³⁾)
