617721444260c5d2db99e650c03263d9bcadfe5d
tapyu <rubem.engenharia@gmail.com>
2022-07-18 06:58:24 -0300

    [ICA]: finish ICA papper

--- phd_ica/Exercises/TC1: Sistemas Fuzzy para Regressão Não Linear/fuzzy_mamdani.jl
+++ phd_ica/Exercises/TC1: Sistemas Fuzzy para Regressão Não Linear/fuzzy_mamdani.jl
@@ -8,12 +8,14 @@ include("inference_mamdani.jl")
 
 # Mamdani fuzzy infer system
 𝐲̂ = rand(length(𝐲))
-for (K, 𝐲_range_max) ∈ zip((2,3), (190,150))
+for (K, 𝐲_range_max, labels) ∈ zip((2,3), (190,150), (["LOW fuzzy set "*L"(A_1)" "MEDIUM fuzzy set"*L"(A_2)" "HIGH fuzzy set"*L"(A_3)"], ["LOW fuzzy set "*L"(A_1)" "HIGH fuzzy set"*L"(A_2)"]))
     𝐲_range = range(0, 𝐲_range_max, length(𝐲)) # Universo de discurso (?) da variavel de saida
     all_μx_A = hcat(map(input_fuzzification, range(minimum(𝐱), maximum(𝐱), length(𝐱)), fill(K, length(𝐱)))...)' # input fuzzification (all domain, all fuzzy set)
     for (k, μx_A_k) ∈ enumerate(eachcol(all_μx_A)) # for each set
         all_μx_A[:,k]/=maximum(μx_A_k) # normalize it to unity
     end
+    local fig = plot(all_μx_A, label=labels, xlabel=L"x\_n", ylabel=L"\mu_A(x_n)")
+    savefig(fig, "figs/mamdani_fuzzy/fuzzification-for-$(K)sets.png")
 
     all_μy_B = hcat(map(output_fuzzification, 𝐲_range, fill(K, length(𝐲)))...)' # output fuzzification (all domain, all fuzzy set)
     all_μy_B/=maximum(all_μy_B) # normalize it to unity
@@ -24,7 +26,7 @@ for (K, 𝐲_range_max) ∈ zip((2,3), (190,150))
 end
 
 # plot the result
-p = scatter(𝐱, 𝐲,
+fig = scatter(𝐱, 𝐲,
         markershape = :hexagon,
         markersize = 4,
         markeralpha = 0.6,
@@ -36,3 +38,22 @@ p = scatter(𝐱, 𝐲,
         ylabel = "Outputs",
         label = "Data")
 plot!(𝐱, 𝐲̂, linewidth=2, label=L"\hat{y}_n")
+savefig(fig, "figs/mamdani_fuzzy/fuzzy_prediction.png")
+
+𝛜 = 𝐲 - 𝐲̂ # residues
+
+# residues statistics
+𝛜̄ = Σ(𝛜)/length(𝛜) # must be approximetly zero
+𝔼𝛜² = Σ(𝛜.^2)/length(𝛜) # second moment (mean squared error, MSE)
+σ̂²ₑ = 𝔼𝛜² - 𝛜̄^2 # σ̂²ₑ≈𝔼𝛜² (variance≈power)
+
+# coefficient of determination
+𝐲̄ = Σ(𝐲)/length(𝐲)
+R² = 1 - (Σ(𝛜.^2)/Σ((𝐲.-𝐲̄).^2))
+
+# Pearson correlation between 𝐲 and 𝐲̂
+ρ = cor(𝐲, 𝐲̂)
+
+println("mean squared error, MSE: $(𝔼𝛜²)")
+println("Coefficient of determination: $(R²)")
+println("Pearson correlation: $(ρ)")
