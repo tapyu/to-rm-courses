5099035aedcc56f33a0fda0f667c0f00ee4a0765
tapyu <rubem.engenharia@gmail.com>
2022-07-25 07:29:54 -0300

    [adapt_filt]: q6 item b

--- phd_filt_adap/Computational exercises/list3/q6a.jl
+++ phd_filt_adap/Computational exercises/list3/q6a.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, DSP, Plots
+using LinearAlgebra, DSP, Plots, LaTeXStrings
 Σ = sum
 
 # system parameters
@@ -29,25 +29,25 @@ end
 
 # equalizer (normalized LMS)
 𝐰₍ₙ₎ = zeros(M) # filter coefficients (initializing)
-𝐞1 = Vector{ComplexF64}(undef, Nₜᵣₙ)
+𝐞 = Vector{ComplexF64}(undef, Nₜᵣₙ)
 for n ∈ 1+δ:Nₜᵣₙ+δ
     𝐱₍ₙ₎ = 𝐱[n:-1:n-δ] # input vector at the instant n -> [x[n], x[n-1], x[n-2], ..., x[n-15]]
     y₍ₙ₎ = 𝐰₍ₙ₎ ⋅ 𝐱₍ₙ₎ # y(n)
     d₍ₙ₎ = 𝐬[n-δ] # d(n) = s[n-δ]
     e₍ₙ₎ = d₍ₙ₎ - y₍ₙ₎
-    𝐞1[n-δ] = e₍ₙ₎
+    𝐞[n-δ] = e₍ₙ₎
     𝐠̂₍ₙ₎ = -2e₍ₙ₎'*𝐱₍ₙ₎ # stochastic gradient
     global 𝐰₍ₙ₎ -= μ*𝐠̂₍ₙ₎/(𝐱₍ₙ₎⋅𝐱₍ₙ₎ + γ)
 end
 
 # DECISION-DIRECTED MODE ###
-N = 5e3 # number of samples for the decision-directed mode
-𝐬 = rand([i[1]+i[2]*im for i in Iterators.product(-3:2:3, -3:2:3)], Nₜᵣₙ+Nₒ+δ) # constellation for 16-QAM
-Pₛ = Σ(abs2.(𝐬))/Nₜᵣₙ # signal power -> 𝔼[‖𝐬‖²]
+N = 5_000 # number of samples for the decision-directed mode
+𝐬 = rand([i[1]+i[2]*im for i in Iterators.product(-3:2:3, -3:2:3)], N+Nₒ+δ) # constellation for 16-QAM
+Pₛ = Σ(abs2.(𝐬))/N # signal power -> 𝔼[‖𝐬‖²]
 
 # channel
-𝐱 = Vector{ComplexF64}(undef, Nₜᵣₙ+Nₒ+δ)
-for n ∈ 1+Nₒ:Nₜᵣₙ+Nₒ+δ
+𝐱 = Vector{ComplexF64}(undef, N+Nₒ+δ)
+for n ∈ 1+Nₒ:N+Nₒ+δ
     𝐱[n] = 0.5𝐬[n] + 1.2𝐬[n-1] + 1.5𝐬[n-2] - 𝐬[n-3]
 end
 # cut off the noncomputed part
@@ -56,7 +56,7 @@ end
 
 # noise
 σ²ₙ = Pₛ*1e-3 # SNR = 30 dB = 10 log(Pₛ/σ²ₙ)
-𝐯 = √(σ²ₙ)*randn(Nₜᵣₙ+δ)
+𝐯 = √(σ²ₙ)*randn(N+δ)
 𝐱 += 𝐯
 
 # equalizer in decision-directed mode
@@ -72,8 +72,8 @@ function hard_decisor(x)
     end
 end
 
-𝐲 = Vector{ComplexF64}(undef, Nₜᵣₙ+δ) # output signal
-for n ∈ 1+δ:Nₜᵣₙ+δ
+𝐲 = Vector{ComplexF64}(undef, N+δ) # output signal
+for n ∈ 1+δ:N+δ
     𝐱₍ₙ₎ = 𝐱[n:-1:n-δ] # input vector at the instant n -> [x[n], x[n-1], x[n-2], ..., x[n-15]]
     y₍ₙ₎ = 𝐰₍ₙ₎ ⋅ 𝐱₍ₙ₎ # y(n)
     # decisor
@@ -82,10 +82,14 @@ end
 
 # ignoring the noncomputed part
 𝐲 = 𝐲[1+δ:end]
+𝐱 = 𝐱[1+δ:end]
 𝐬 = 𝐬[1:end-δ]
 
-# plot([real(𝐲) real(𝐬); imag(𝐲) imag(𝐬)], layout=(2,1), size=(1200,800))
+# save signal output
+p = [plot([real(𝐲[end-50:end]) real(𝐬[end-50:end]) real(𝐱[end-50:end])], line=:stem, markershape=[:star5 :utriangle :circle], title="Real part", label=[L"\hat{s}(n)" L"s(n)" L"x(n)"]) plot([imag(𝐲[end-50:end]) imag(𝐬[end-50:end]) imag(𝐱[end-50:end])], line=:stem, title="Imaginary part", markershape=[:star5 :utriangle :circle], label=[L"\hat{s}(n)" L"s(n)" L"x(n)"])]
+fig = plot(p..., size=(900,600), layout=(2,1))
+savefig(fig, "list3/figs/q6a_output.png")
 
-plot([real(𝐲[end-150:end]) real(𝐬[end-150:end])], size=(1200,800), line=:stem, marker=:xcross)
-
-# plot(abs.(𝐞1))
+# save error output
+fig = plot(abs2.(𝐞), title="Signal error in training phase for $(Nₜᵣₙ) samples", label=L"\mid e(n)\mid^2", xlabel="n")
+savefig(fig, "list3/figs/q6a_error.png")
