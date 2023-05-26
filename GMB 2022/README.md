ee59313c51c1fab3aec5a838af5ad351df37ae0f
tapyu <rubem.engenharia@gmail.com>
2023-05-25 23:11:57 -0300

    add HW from others

---
+++ GMB 2022/README.md
@@ -0,0 +1,3 @@
+## Notes about the recordings and software
+
+They are too large to host on GitHub, you can find them on Google Dive on .engenharia email.
--- phd_rec_padr/TC1/tictoc.m
+++
@@ -1,7 +0,0 @@
-table = readtable('data.csv');
-X = table2array(table(:,1:end-1));
-tic
-cov(X);
-elapsed_time = toc;
-
-save('tictoc.mat', 'elapsed_time');
