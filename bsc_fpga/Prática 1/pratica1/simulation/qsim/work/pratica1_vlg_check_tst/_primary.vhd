0c2bc9302ddd89c4b9f6b859e1c0e3a0135b7e07
tapyu <rubem.engenharia@gmail.com>
2023-05-30 18:35:22 -0300

    retreive fpga_bsc course
    Former-commit-id: b2f859517d8d9104fb767948b7e58580528280fd

--- bsc_fpga/Prática 1/pratica1/simulation/qsim/work/pratica1_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity pratica1_vlg_check_tst is
-    port(
-        f               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end pratica1_vlg_check_tst;
--- bsc_fpga/Prática 3/pratica3/simulation/qsim/work/registrador_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity registrador_vlg_vec_tst is
-end registrador_vlg_vec_tst;
